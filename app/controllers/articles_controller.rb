be1f758b5dfd112931b5ad8c4a02a2ea94260b7a
laura-paullette <laura.paullette@student.moringaschool.com>
2023-06-27 12:47:17 +0300

    completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,13 @@ class ArticlesController < ApplicationController
 
   def show
     article = Article.find(params[:id])
-    render json: article
+    session[:page_views] ||= 0
+    session[:page_views] += 1
+    if session[:page_views] <= 3
+      render json: article
+    else
+      unauthorized
+    end
   end
 
   private
@@ -17,4 +23,7 @@ class ArticlesController < ApplicationController
     render json: { error: "Article not found" }, status: :not_found
   end
 
+  def unauthorized
+    render json: { error: "Maximum pageview limit reached" }, status: :unauthorized
+  end
 end
